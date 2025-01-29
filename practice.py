ef4c0fd62eaa1762df78d9cbdc8237277ba30f33
Dhruvi Ladani <dhruvi.ladani@inferenz.local>
2025-01-29 18:00:46 +0530

    learning python..

--- practice.py
+++ practice.py
@@ -7,3 +7,24 @@ for i in range(n):
 print(l)
 
 
+
+# n = 3 
+# l = list()
+# for i in range(n):
+#     l.append(input())
+
+# print(l)
+
+
+
+def sum(a, b=4):
+    s = a + b
+    return s
+
+print(sum(1))
+
+# def fun():
+#     print("hello")
+
+# out = fun()
+# print(out)
